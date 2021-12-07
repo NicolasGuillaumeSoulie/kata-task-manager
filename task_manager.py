1cea7e48f735ca623d565e66ee0e36ac57162f3c
Nicolas Guillaume SOULIÉ <nicolas.guillaume.soulie@efrei.net>
2021-12-07 17:13:16 +0100

    loop

--- task_manager.py
+++ task_manager.py
@@ -1,21 +1,21 @@
 import unittest
 
-def add(tasks, desc):
-	lastId = 1 if len(tasks)<1 else max(tasks.keys()) + 1
-	tasks[lastId] = Task(lastId, desc)
+def add(tasksMan, desc):
+	lastId = 1 if len(tasksMan.tasks)<1 else max(tasksMan.tasks.keys()) + 1
+	tasksMan.tasks[lastId] = Task(lastId, desc)
 
-def remove(tasks, id):
+def remove(tasksMan, id):
 	print(int(id))
-	del tasks[int(id)]
+	del tasksMan.tasks[int(id)]
 
-def done(tasks, id):
-	tasks[int(id)].status = "done"
+def done(tasksMan, id):
+	tasksMan.tasks[int(id)].status = "done"
 
-def toDo(tasks, id):
-	tasks[int(id)].status = "to do"
+def toDo(tasksMan, id):
+	tasksMan.tasks[int(id)].status = "to do"
 
-def quit():
-	return
+def quit(tasksMan, osef):
+	tasksMan.exit = True
 
 parser = {
 	"+" : add,
@@ -38,17 +38,31 @@ class Task:
 class TaskManager:
 	def __init__(self):
 		self.tasks = {}
+		self.exit = False
 
 	def cmd(self, inValue = input):
 		inCmd = inValue()
-		parser[inCmd[0]](self.tasks, inCmd[2::])
+		parser[inCmd[0]](self, inCmd[2::])
 
 	def display(self):
 		cmdString = ""
 		for task in self.tasks.values():
 			cmdString += task.toString() + "\n"
-		print(cmdString)
 		return cmdString
+
+	def loop(self, inValues = input):
+		self.exit = False 
+		while not self.exit:
+			self.cmd(inValues)
+			print(self.display())
+
+fakeInputs = ["+ Learn Python", "+ Learn TDD", "+ Learn SQL", "x 2", "q"]
+
+def fakeConsole():
+	fakeIn = fakeInputs[0] + ""
+	del fakeInputs[0]
+	print(fakeIn)
+	return fakeIn
 
 class TestTaskManager(unittest.TestCase):
 
@@ -109,6 +123,10 @@ class TestTaskManager(unittest.TestCase):
     	tm.cmd(lambda : "+ Learn SQL")
     	self.assertEqual(tm.display(), "1 [ ] Learn Python\n2 [ ] Learn TDD\n3 [ ] Learn SQL\n")
 
+    def test_loop(self):
+    	tm = TaskManager()
+    	tm.loop(fakeConsole)
+    	self.assertEqual(tm.display(), "1 [ ] Learn Python\n2 [x] Learn TDD\n3 [ ] Learn SQL\n")
 
 if __name__ == '__main__':
     unittest.main()
